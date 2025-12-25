3a46cf76ca30c5ee2a9f6d52cb6a5b8c2f72eb99
Arnob-HRes <arnobsarkersupta.education@gmail.com>
2025-12-25 22:36:15 +0600

    WT Project HOME PAGE 1

---
+++ HOME_PAGE.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>
+            Local Festival Stall Allocation & Fee Management System
+        </title>
+    </head>
+    <style>
+        #F1{
+            position: absolute;
+            top: 280px;
+            right: 150px;
+            width: 500px;
+            height: auto;
+            text-align: center;
+        }
+        #B1{
+            width: 300px;
+            height: 50px;
+            text-align: center;
+            color: white;
+            background-color: blue;
+            border: 1px solid blue;
+            border-radius: 10px;
+            margin:20px;
+        }
+        #B1:hover{
+            background-color: royalblue;
+        }
+        #B2{
+            width: 300px;
+            height: 50px;
+            text-align: center;
+            color: blue;
+            background-color: lightblue;
+            border: 1px solid lightblue;
+            border-radius: 10px;
+            margin:20px;
+        }
+        #B2:hover{
+            background-color: royalblue;
+            color: white;
+        }
+        #D1{
+            position: absolute;
+            top:50px;
+            right: 190px;
+            background-color: white;
+            width: auto;
+            height: auto;
+        }
+        #D2{
+            position: absolute;
+            top:120px;
+            right: 210px;
+            background-color: white;
+            width: auto;
+            height: auto;
+        }
+        #D3{
+            position: absolute;
+            top: 0px;
+            left: 0px;
+            background-color: blue;
+            border-radius: 20px;
+            margin: 5px;
+            width: 750px;
+            height: 720px;
+        }
+        #T1{
+            position: absolute;
+            top: 640px;
+            right: 330px;
+        }
+        #IMG1{
+            position: absolute;
+            top: 0px;
+            left: 0px;
+            z-index: -1;
+        }
+    </style>
+    <body>
+        <img src="—Pngtree—geometric pattern white gold minimalist_1575961.png" alt="Background" id="IMG1">
+        <div id="D3"></div>
+        <div id="D1">
+            <h1 style="font-size: 35px;">Local Festival Stall Allocation</h1>
+        </div>
+        <div id="D2">
+            <h1 style="font-size: 35px;">& Fee Management System</h1>
+        </div>
+        <form id="F1">
+            <hr>
+            <button id="B1">
+                LOGIN
+            </button>
+            <button id="B2">
+                SIN UP
+            </button> <br>
+            <hr>
+        </form>
+        <h4 id="T1">@ARNOB SARKER SUPTA & HRIDRITA SAHA MOON 2025</h4>
+    </body>
+</html>
