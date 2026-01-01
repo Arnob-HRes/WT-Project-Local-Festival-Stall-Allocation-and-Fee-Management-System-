c7904fd5ff2b1d963bc0d8669c4c241bb437418e
Arnob-HRes <arnobsarkersupta.education@gmail.com>
2026-01-01 18:35:41 +0600

    WT Project Home page Information

--- Stall Renter/MVC/View Files/HOME_PAGE.php
+++ Stall Renter/MVC/View Files/HOME_PAGE.php
@@ -2,185 +2,16 @@
 <html>
     <head>
         <title>
-            Local Festival Stall Allocation & Fee Management System
+            Home Page
         </title>
+        <link rel="stylesheet" href="../Stylesheets/HOME_PAGE.css">
     </head>
-    <style>
-        #F1{
-            position: absolute;
-            top: 220px;
-            right: 390px;
-            width: 650px;
-            background-color: transparent;
-            backdrop-filter: blur(8px);
-            border-radius: 50px;
-            border:1px solid white;
-            padding: 50px;
-            height: auto;
-            text-align: center;
-        }
-        #B1{
-            width: 300px;
-            height: 50px;
-            text-align: center;
-            color: white;
-            background-color: blue;
-            border: 1px solid blue;
-            border-radius: 10px;
-            margin:20px;
-        }
-        #B1:hover{
-            background-color: royalblue;
-        }
-        #B2{
-            width: 300px;
-            height: 50px;
-            text-align: center;
-            color: blue;
-            background-color: lightblue;
-            border: 1px solid lightblue;
-            border-radius: 10px;
-            margin:20px;
-        }
-        #B2:hover{
-            background-color: royalblue;
-            color: white;
-        }
-        #D1{
-            background-color: transparent;
-            width: auto;
-            height: auto;
-        }
-        #D2{
-            background-color: transparent;
-            width: auto;
-            height: auto;
-        }
-        #T1{
-            position: absolute;
-            top: 810px;
-            right: 190px;
-        }
-        #d4{
-            position: absolute;
-            top: 850px;
-        }
-        #d5{
-            position: absolute;
-            top: 1200px;
-        }
-        #d6{
-            position: absolute;
-            top: 1550px;
-        }
-        #d7{
-            position: absolute;
-            top: 1900px;
-        }
-        #d3{
-            position: absolute;
-            top: 2250px;
-        }
-        .DIV{
-            position: relative;
-            margin: 50px 0px;
-            background-color: white;
-            border: 3px solid black;
-            border-radius: 20px;
-            width: 1350px;
-            height: 300px;
-        }
-        .d-sub{
-            width: 400px;
-            height: 260px;
-            margin: 20px;
-            background-color: blue;
-            border-radius: 10px;
-        }
-        #d5-sub{
-            position: absolute;
-            right: 0px;
-        }
-        #d7-sub{
-            position: absolute;
-            right: 0px;
-        }
-        #Head{
-            position: fixed;
-            top: 0px;
-            left: 1px;
-            width: 1510px;
-            height: 100px;
-            margin: 0px 1px 1px 1px;
-            background-color: transparent;
-            backdrop-filter: blur(10px);
-            border: 1px solid white;
-            border-bottom-right-radius: 10px;
-            border-bottom-left-radius: 10px;
-            z-index: 1;
-            overflow: hidden;
-        }
-        #B3{
-            position: absolute;
-            right: 210px;
-            width: 200px;
-            height: 50px;
-            text-align: center;
-            color: white;
-            background-color: blue;
-            border: 1px solid blue;
-            border-radius: 10px;
-            margin:25px 10px 25px 0px;
-        }
-        #B3:hover{
-            background-color: royalblue;
-        }
-        #B4{
-            position: absolute;
-            right: 0px;
-            width: 200px;
-            height: 50px;
-            text-align: center;
-            color: blue;
-            background-color: white;
-            border: 1px solid white;
-            border-radius: 10px;
-            margin:25px 10px 25px 0px;
-        }
-        #B4:hover{
-            background-color: royalblue;
-            color: white;
-        }
-        #img1{
-            position: absolute;
-            top: 0px;
-            left: 0px;
-            width: 100%;
-            height: 100%;
-            object-fit: cover;
-            z-index: -1;
-        }
-        #IMG3{
-            position: absolute;
-            top: 0px;
-            left: 0px;
-            width: 1525px;
-            height: 850px;
-            object-fit: cover;
-            z-index: -1;
-        }
-        #IMG4{
-            position: absolute;
-            top:20px;
-            left: 10px;
-            width: 60px;
-            height: 60px;
-            border-radius: 20px;
-        }
-    </style>
     <body>
         <img src="../Images/HOME_PAGE_BACKGROUND.jpg" alt="Home Page" id="IMG3">
         <header id="Head">
+            <a href="HOME_PAGE.php" id="S1" class="Sec">Home</a>
+            <a href="#d4" id="S2" class="Sec">Information</a>
+            <a href="#footer" id="S3" class="Sec">About</a>
             <input type="button" value="SING IN" id="B3" onclick="window.location.href='LOGIN_PAGE.php'">
             <input type="button" value="SING UP" id="B4" onclick="window.location.href='LOGIN_PAGE.php'">
             <img src="../Images/LOGO.png" alt="LOGO" id="IMG4">
@@ -197,11 +28,51 @@
             <input type="button" value="SING UP" id="B2" onclick="window.location.href='LOGIN_PAGE.php'"><br>
             <hr>
         </form>
-        <h4 id="T1">©ARNOB SARKER SUPTA & HRIDRITA SAHA MOON 2025</h4>
-        <div id="d3" class="DIV"><div id="d3-sub" class="d-sub">></div></div>
-        <div id="d4" class="DIV"><div id="d4-sub" class="d-sub">></div></div>
-        <div id="d5" class="DIV"><div id="d5-sub" class="d-sub">></div></div>
-        <div id="d6" class="DIV"><div id="d6-sub" class="d-sub">></div></div>
-        <div id="d7" class="DIV"><div id="d7-sub" class="d-sub">></div></div>
+        <div id="d3" class="DIV">
+            <h2 style="font-family: 'Baloo 2', cursive;color: black" id ="d3-title">Arcade Stall</h2>
+            <p id="p5">At a thrilling festival, arcade stalls pulse with excitement and nostalgic fun, drawing crowds to brightly lit booths packed with classic carnival games that test skill and luck. Challenge yourself at ring toss for precision, pop balloons with darts for a satisfying burst, shoot hoops in timed basketball challenges, or knock down targets in knockdown games—all under flashing lights and cheerful music. Walls and shelves overflow with tempting prizes, from giant plush teddy bears and colorful stuffed animals to quirky toys and novelty items that make perfect wins. These lively stalls are hubs of laughter and friendly competition, where families and friends cheer each other on, creating unforgettable moments and taking home fluffy souvenirs of victory.</p>
+            <div id="d3-sub" class="d-sub">
+                <img src="../Images/Arcade_Stall.png" alt="Arcade" id="Arcade">
+            </div>
+        </div>
+        <div id="d4" class="DIV">
+            <h2 style="font-family: 'Baloo 2', cursive;color: black" id ="d4-title">Food Stall</h2>
+            <p id="p1">Festival food stalls offer a vibrant array of quick, flavorful options to keep attendees energized. From juicy gourmet burgers with premium toppings to zesty street tacos bursting with fresh salsas, wood-fired pizzas baked on-site, indulgent desserts like loaded waffles and gourmet popcorn, and global fusion bites such as steamed bao buns and loaded fries, these stalls deliver portable, sensory delights that perfectly complement the festive atmosphere.</p>
+            <div id="d4-sub" class="d-sub">
+                <img src="../Images/Food_Stall.jpg" alt="Food Stall" id="FoodStall">
+            </div>
+        </div>
+        <div id="d5" class="DIV">
+            <h2 style="font-family: 'Baloo 2', cursive;color: black" id ="d5-title">Art Stall</h2>
+            <p id="p2">At a lively festival, art stalls transform the grounds into a colorful gallery of creativity, showcasing the talents of local and independent artists in vibrant, inviting booths. Browse stunning original paintings ranging from abstract explosions of color to serene landscapes that capture the essence of nature. Discover intricate handmade jewelry—sparkling earrings, beaded necklaces, and unique rings crafted with care from metals, gems, and recycled materials. Admire whimsical sculptures, pottery, and ceramics that add a touch of artistry to any home, alongside textiles like hand-dyed scarves, woven baskets, and printed fabrics bursting with patterns. These stalls offer one-of-a-kind treasures, perfect for souvenirs or gifts, where you can chat with the makers, learn their stories, and take home a piece of the festival's inspiring spirit.</p>
+            <div id="d5-sub" class="d-sub">
+                <img src="../Images/Arts_stall.png" alt="Arts Stall" id="ArtStall">
+            </div>
+        </div>
+        <div id="d6" class="DIV"><div id="d6-sub" class="d-sub">
+             <h2 style="font-family: 'Baloo 2', cursive;color: black" id ="d6-title">Toys and Accessories Stall</h2>
+            <p id="p3">At a bustling festival, toys and accessories stalls burst with playful energy and whimsical charm, delighting visitors of all ages with colorful displays of fun and flair. Discover shelves overflowing with handmade plush toys, cuddly stuffed animals in every shape and color, wooden puzzles, spinning tops, and eco-friendly games that spark imagination and joy. Alongside, racks shimmer with trendy accessories—boho hats, beaded bracelets, quirky sunglasses, festival-ready glitter face gems, patterned scarves, and unique bags perfect for carrying your festival finds. These vibrant booths offer affordable, one-of-a-kind treasures and souvenirs, where kids (and kids at heart) can pick out the perfect memento to remember the day's magic.</p>
+            <div id="d4-sub" class="d-sub">
+                <img src="../Images/Toys ans Accessories_Stall.png" alt="Food Stall" id="TandAStall">
+            </div>
+        </div></div>
+        <div id="d7" class="DIV">
+            <h2 style="font-family: 'Baloo 2', cursive;color: black" id ="d7-title">Hand Craft Stall</h2>
+            <p id="p4">At a charming festival, handicraft stalls radiate warmth and authenticity, brimming with the heartfelt creations of skilled artisans in beautifully arranged displays. Explore tables laden with exquisite handmade jewelry—delicate earrings, layered necklaces, and bespoke bracelets crafted from natural stones, silver, and beads. Admire finely thrown pottery and ceramics in earthy glazes, scented artisanal soaps and candles in soothing fragrances, intricately woven textiles like scarves and bags, carved wooden ornaments, and unique home decor items that tell stories of tradition and innovation. These inviting booths are treasure troves of sustainable, one-of-a-kind pieces, where you can meet the makers, hear their inspirations, and find the perfect handmade souvenir to cherish long after the festival ends.</p>
+            <div id="d7-sub" class="d-sub">
+                <img src="../Images/Hand Craft_Stall.png" alt="Hand Craft Stall" id="HandStall">
+            </div>
+        </div>
+
+        <footer id="footer">
+            <h2>Local Festival Stall Allocation & Fee Management System</h2>
+            <img src="../Images/Footer.jpg" alt="Footer" id=BackFooter>
+            <img src="../Images/LOGO.png" alt="" id="FooterLogo">
+            <a href="https://www.facebook.com/share/1DA2H2WT9q/" id="AF">Arnob's Facebook</a>
+            <a href="https://www.facebook.com/share/1ASuv8pzxi/" id="HF">Hridita's Facebook</a>
+            <a href="https://github.com/Arnob-HRes">Arnob's GitHub</a>
+            <a href="">Hridita's GitHub</a>
+            <h4 id="T1">©ARNOB SARKER SUPTA & HRIDRITA SAHA MOON 2025</h4>
+        </footer>
     </body>
 </html>
