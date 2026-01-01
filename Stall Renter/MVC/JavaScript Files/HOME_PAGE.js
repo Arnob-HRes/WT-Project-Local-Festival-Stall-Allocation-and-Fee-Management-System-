4255c85eccc2d2d0f5d2ae5931b1444fefd7e80f
Arnob-HRes <arnobsarkersupta.education@gmail.com>
2026-01-01 19:48:24 +0600

    WT Project Home page js Modification

--- Stall Renter/MVC/JavaScript Files/HOME_PAGE.js
+++ Stall Renter/MVC/JavaScript Files/HOME_PAGE.js
@@ -1,10 +1,1 @@
 alert("Welcom To The home page of Local Festival Stall Allocation & Fee Management System");
-window.onscroll=function(){
-    let C=document.getElementById("footer");
-    if(document.body.scrollTop >= 850 || document.documentElement.scrollTop >=850 ){
-        C.classList.add("scrolled");
-    }
-    else{
-        C.classList.remove("scrolled");
-    }
-}
