87c2d64a2232040c1aa43f9e433788bb375856f0
Arnob-HRes <arnobsarkersupta.education@gmail.com>
2026-01-01 19:57:00 +0600

    WT Project Home Page Meta Tag Modification

--- Stall Renter/MVC/View Files/HOME_PAGE.php
+++ Stall Renter/MVC/View Files/HOME_PAGE.php
@@ -8,7 +8,7 @@
         <meta name="author" content="Arnob Sarker Supta and Hridrita Saha Moon">
         <meta name="keywords" content="Project, WT, JS, CSS, HTML, AIUB, CSE, Web Technology">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta http-equiv="refresh" content="6000">
+        <meta http-equiv="refresh" content="1800">
         <link rel="stylesheet" href="../Stylesheets/HOME_PAGE.css">
     </head>
     <body>
