e6b8bc732d0e7643a32cc253dc53787cf78409da
Arnob-HRes <arnobsarkersupta.education@gmail.com>
2026-01-01 21:48:22 +0600

    WT Project Login Page meta tag adding

---
+++ Stall Renter/MVC/View Files/LOGIN_PAGE.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<html>
+    <head>
+        <title>
+            LOGIN Page
+        </title>
+        <meta name="description" content="This is a Web site for Local Festival Stall Allocation & Fee Management System" >
+        <meta name="author" content="Arnob Sarker Supta and Hridrita Saha Moon">
+        <meta name="keywords" content="Project, WT, JS, CSS, HTML, AIUB, CSE, Web Technology">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="refresh" content="1800">
+        <link rel="stylesheet" href="../Stylesheets/LOGIN_PAGE.css">
+    </head>
+</html>
