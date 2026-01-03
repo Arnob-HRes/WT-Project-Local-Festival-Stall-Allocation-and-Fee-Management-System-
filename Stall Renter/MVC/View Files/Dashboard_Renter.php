812e4d495e1b996bcced07431eb45390b86d373f
Arnob-HRes <arnobsarkersupta.education@gmail.com>
2026-01-03 19:36:41 +0600

    WT Project Dashboard Page background

---
+++ Stall Renter/MVC/View Files/Dashboard_Renter.php
@@ -0,0 +1,25 @@
+<?php
+include("../Controller Logic/Dashboard_Renter_Valid.php");
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<html>
+    <head>
+        <title>
+            Ranter Dashboard
+        </title>
+        <meta name="description" content="This is a Web site for Local Festival Stall Allocation & Fee Management System" >
+        <meta name="author" content="Arnob Sarker Supta and Hridrita Saha Moon">
+        <meta name="keywords" content="Project, WT, JS, CSS, HTML, AIUB, CSE, Web Technology">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="refresh" content="1800">
+        <link rel="stylesheet" href="../Stylesheets/Dashboard_Renter.css">
+    </head>
+    <body>
+
+        <img src="../Images/Dashboard_background.jpg" alt="Dashborad Background" id="mainimg">
+        
+        <script src="../JavaScript Files/Dashboard_Renter.js"></script>
+    </body>
+</html>
