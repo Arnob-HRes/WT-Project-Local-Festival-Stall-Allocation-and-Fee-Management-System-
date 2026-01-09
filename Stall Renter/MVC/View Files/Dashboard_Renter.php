062e823b6f5c6e37528f67597e555d385eda4091
Arnob-HRes <arnobsarkersupta.education@gmail.com>
2026-01-09 16:15:02 +0600

    dashboard footer

--- Stall Renter/MVC/View Files/Dashboard_Renter.php
+++ Stall Renter/MVC/View Files/Dashboard_Renter.php
@@ -95,7 +95,12 @@ include("../JavaScript Files/Dashboard_Renter_js.php");
         </center>
         <hr id="h4">
         <footer id="footer">
-            
+            <img src="../Images/LOGO.png" alt="" id="FooterLogo">
+            <a href="https://www.facebook.com/share/1DA2H2WT9q/" id="AF" class="fl">Arnob's Facebook</a>
+            <a href="https://www.facebook.com/share/1ASuv8pzxi/" id="HF" class="fl">Hridita's Facebook</a>
+            <a href="https://github.com/Arnob-HRes" id="AG" class="fl">Arnob's GitHub</a>
+            <a href="https://github.com/Hridrita" id="HG" class="fl">Hridita's GitHub</a>
+            <h5 id="T1">©ARNOB SARKER SUPTA & HRIDRITA SAHA MOON 2025</h5>
         </footer>
     </body>
 </html>
