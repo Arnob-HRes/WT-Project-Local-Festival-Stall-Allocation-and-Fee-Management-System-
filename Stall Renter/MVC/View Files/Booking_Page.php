3d720c40548b937ab6007a648340bc559cf6bd46
Arnob-HRes <arnobsarkersupta.education@gmail.com>
2026-01-12 21:31:01 +0600

    Booking page

---
+++ Stall Renter/MVC/View Files/Booking_Page.php
@@ -0,0 +1,25 @@
+<?php
+include("../Controller Logic/Booking_Page_Valid.php");
+include("../Controller Logic/session_and_cookie_check.php");
+include("../Stylesheets/Booking_Page_css.php");
+include("../JavaScript Files/Booking_Page_js.php");
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<html>
+    <head>
+        <title>
+            Booking Page
+        </title>
+        <meta name="description" content="This is a Web site for Local Festival Stall Allocation & Fee Management System" >
+        <meta name="author" content="Arnob Sarker Supta and Hridrita Saha Moon">
+        <meta name="keywords" content="Project, WT, JS, CSS, HTML, AIUB, CSE, Web Technology">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="refresh" content="1800">
+    </head>
+    <body>
+        <img src="../Images/BookingSide.gif" alt="Booking" id="IMG1">
+        <img src="../Images/booking.gif" alt="BookingTop" id=IMG2>
+    </body>
+</html>
