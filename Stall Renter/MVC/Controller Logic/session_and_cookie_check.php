d50b65aec3a19e4f2e222c8d26bc5441a932b99d
Arnob-HRes <arnobsarkersupta.education@gmail.com>
2026-01-12 21:51:25 +0600

    Session fix

--- Stall Renter/MVC/Controller Logic/session_and_cookie_check.php
+++ Stall Renter/MVC/Controller Logic/session_and_cookie_check.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 if(!isset($_SESSION["username"])){
-    header("Location:../Views Files/HOME_PAGE.php");
+    header("Location:../View Files/HOME_PAGE.php");
     exit();
 }
 ?>
