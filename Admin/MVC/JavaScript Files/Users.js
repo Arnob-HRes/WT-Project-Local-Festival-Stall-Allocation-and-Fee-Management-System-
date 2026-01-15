d6563972ee8587ce3639c4d580f1908ca409f5d7
WT_FALL-25-26- <moonsaha721@gmail.com>
2026-01-15 22:00:42 +0600

    modified css file

--- Admin/MVC/JavaScript Files/Users.js
+++ Admin/MVC/JavaScript Files/Users.js
@@ -1,6 +1,5 @@
   const addUserBtn = document.getElementById('addUserBtn');
-  const layout     = document.getElementById('usersLayout');document.getElementById('usersLayout');
-
+  const layout     = document.getElementById('usersLayout');
   addUserBtn.addEventListener('click', () => {
     layout.classList.toggle('form-open');
   });
