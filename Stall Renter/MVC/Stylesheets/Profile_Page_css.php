6a88277ea75aefcf4dd3c8e8ca416c84042db5d1
Arnob-HRes <arnobsarkersupta.education@gmail.com>
2026-01-16 21:09:20 +0600

    Profile Page 2

---
+++ Stall Renter/MVC/Stylesheets/Profile_Page_css.php
@@ -0,0 +1,72 @@
+<style>
+    #PPMIMG{
+        position: absolute;
+        top: 0px;
+        left: 0px;
+        width: 1530px;
+        height: 400px;
+        z-index: -1;
+    }
+    #footer{
+    position: absolute;
+    top: 800px;
+    left: 0px;
+    width: 1521px;
+    height: 200px;
+    background-color: black;
+    border-top-left-radius: 20px;
+    border-top-right-radius: 20px;
+    }
+    #FooterLogo
+    {
+            position: absolute;
+            top: 30px;
+            right: 80px;
+            border-radius: 20px;
+            width: 150px;
+            height: 150px;
+    }
+     .fl{
+            font-size: 15px;
+            display: inline-block;
+            padding: 20px;
+            border-radius: 10px;
+            position: absolute;
+            background-color: rgba(255, 255, 255, 0.6);
+            color: Black;
+            text-decoration: none;
+        }
+        .fl:hover{
+            background-color: rgba(255, 255, 255, 0.3);
+        }
+        #AF{
+            top:110px;
+            width: 150px;
+            height: 20px;
+            left: 10px;
+        }
+        #HF{
+            top:110px;
+            width: 120px;
+            height: 20px;
+            left: 250px;
+        }
+        #AG{
+            top:110px;
+            width: 100px;
+            height: 20px;
+            left: 490px;
+        }
+        #HG{
+            top:110px;
+            width: 100px;
+            height: 20px;
+            left: 730px;
+        }
+        #T1{
+            color: white;
+            position: absolute;
+            top: 20px;
+            left: 10px;
+        }
+</style>
---
+++ Stall Renter/MVC/View Files/Profile_Page.php
@@ -0,0 +1,40 @@
+<?php
+include("../Controller Logic/Profile_Page_Valid.php");
+//include("../Controller Logic/session_and_cookie_check.php");
+include("../Stylesheets/Profile_Page_css.php");
+include("../JavaScript Files/Profile_Page_js.php");
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<html>
+    <head>
+        <title>
+            Ranter Dashboard
+        </title>
+        <meta name="description" content="This is a Web site for Local Festival Stall Allocation & Fee Management System" >
+        <meta name="author" content="Arnob Sarker Supta and Hridrita Saha Moon">
+        <meta name="keywords" content="Project, WT, JS, CSS, HTML, AIUB, CSE, Web Technology">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="refresh" content="1800">
+    </head>
+    <body>
+        <img src="../Images/Profile_Page.gif" alt="Profile Page" id="PPMIMG">
+
+        
+
+
+
+
+
+
+        <footer id="footer">
+            <img src="../Images/LOGO.png" alt="" id="FooterLogo">
+            <a href="https://www.facebook.com/share/1DA2H2WT9q/" id="AF" class="fl">Arnob's Facebook</a>
+            <a href="https://www.facebook.com/share/1ASuv8pzxi/" id="HF" class="fl">Hridita's Facebook</a>
+            <a href="https://github.com/Arnob-HRes" id="AG" class="fl">Arnob's GitHub</a>
+            <a href="https://github.com/Hridrita" id="HG" class="fl">Hridita's GitHub</a>
+            <h5 id="T1">© ARNOB SARKER SUPTA & HRIDRITA SAHA MOON 2025</h5>
+        </footer>
+    </body>
+</html>
