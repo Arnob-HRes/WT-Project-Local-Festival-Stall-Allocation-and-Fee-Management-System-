f9c10c75355af744af736d3f8c1a7a8a1ef9debe
Arnob-HRes <arnobsarkersupta.education@gmail.com>
2026-01-18 01:46:55 +0600

    setcookie check

--- Stall Renter/MVC/Controller Logic/session_and_cookie_check.php
+++ Stall Renter/MVC/Controller Logic/session_and_cookie_check.php
@@ -4,4 +4,8 @@ if(!isset($_SESSION["username"])){
     header("Location:../View Files/HOME_PAGE.php");
     exit();
 }
+if(!isset($_COOKIE["username"])){
+    header("Location:../View Files/HOME_PAGE.php");
+    exit();
+}
 ?>
